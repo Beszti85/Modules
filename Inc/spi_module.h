ef9cb2a07dbbfcfc9696e98d314661417ae727ab
Beszti85 <csababesztercei@gmail.com>
2023-03-06 03:13:02 +0100

    Add character set to spi disp module

--- Inc/spi_module.h
+++ Inc/spi_module.h
@@ -10,8 +10,16 @@
 
 #include "main.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void SPIMODULE_Init( SPI_HandleTypeDef* ptrSpi, GPIO_TypeDef* ptrGpioPort, uint16_t gpioPin  );
 void SPIMODULE_LedsSetState( uint8_t ledState );
 void SPIMODULE_ToogleDisplay( uint8_t ledState );
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* INC_SPI_MODULE_H_ */
--- Src/spi_module.c
+++ Src/spi_module.c
@@ -7,7 +7,13 @@
 
 #include "mcp23s17.h"
 
+// Display segments cathods
+#define SEG1_ON  0x
+
 static MCP23S17_Handler_t SpiHandler;
+
+// Display segments connected to MCP23S17 Port B
+static uint8_t Disp7SegCharSet[16u] = { 0xF6u, 0x22u, 0xB9u, 0xABu, 0x63u, 0xCBu, 0xDBu, 0xA2u, 0xFBu, 0xEBu, 0xF3u, 0x5Bu, 0xD8u, 0x3Bu, 0xD9u, 0xD1u };
 
 void SPIMODULE_Init( SPI_HandleTypeDef* ptrSpi, GPIO_TypeDef* ptrGpioPort, uint16_t gpioPin  )
 {
@@ -45,3 +51,9 @@ void SPIMODULE_ToogleDisplay( uint8_t ledState )
     MCP23S17_WritePortA( &SpiHandler, 0x00u );
   }
 }
+
+void SPIMODULE_DisplayCharTest( void )
+{
+
+}
+
